17b09b3838015448a4854b1fc6d00f1583687866
Nicholas Berlette <nick@berlette.com>
2022-10-03 22:25:35 -0700

    feat: add basic unit tests

---
+++ test/main_test.ts
@@ -0,0 +1,22 @@
+import "./deps.ts";
+import etag from "../mod.ts";
+import { is } from "../helpers.ts";
+
+describe("etag module default import", () => {
+  it("should generate a deterministic eTag", () =>
+    assertEquals(etag("deno911"), '"7-MjJkMWZlOWM5ZDFmOWI3OGQ0YzR"'));
+
+  it("should also encode with the .encode method", () =>
+    assertEquals(etag.encode("deno911"), '"7-MjJkMWZlOWM5ZDFmOWI3OGQ0YzR"'))
+
+  it("should return [object ETag] for its toStringTag", () =>
+    assertEquals({}.toString.call(etag), "[object ETag]"));
+
+  it("should have callable methods", () => {
+    assertEquals(is(etag.encode), "Function");
+    assertEquals(is(etag.decode), "Function");
+    assertEquals(is(etag.ifMatch), "Function");
+    assertEquals(is(etag.ifNoneMatch), "Function");
+    assertEquals(is(etag.factory), "Function");
+  });
+});
