30eec11f4640696b1a2883c900e3e6502b8bce22
Nicholas Berlette <nick@berlette.com>
2022-12-24 12:48:46 -0800

    refactor: utilities module (_util.ts)
    - move internal decode functions from mod.ts to _util.ts
    - move ETAG_* constants into _util.ts
    - remove useless and hideous augmentation of the dis module
    - clean up some things

--- _util.ts
+++ _util.ts
@@ -1,136 +1,51 @@
 import {
-  type Assert,
-  assert as $assert,
-  AssertionError,
+  assert,
   base64,
   crypto,
   ERR_STREAM_ALREADY_FINISHED,
   ERR_STREAM_DESTROYED,
-  is as $is,
+  is,
   Stats,
   statSync,
 } from "./deps.ts";
 
 import type {
-  Class,
   Context,
   DigestAlgorithm,
   Entity,
   FileInfo,
-  Noop,
   State,
   VirtualFileInfo,
   VirtualFsFile,
 } from "./mod.d.ts";
 
-type AssertOptions = { multipleValues?: boolean };
+export { assert, is };
 
-type Clone<P extends {}> =
-  | P extends infer T
-    ? { -readonly [K in keyof T]: T[K]; }
-  : never;
+export const ETAG_EMPTY = `"0-2jmj7l5rSw0yVb/vlWAYkK/YBwk="`;
+export const ETAG_LENGTH = 27, ETAG_RADIX = 16, ETAG_MTIME_LENGTH = 12;
 
-// define the shape of our modified assert object
-// we'll be adding a callable signature as the default assert(),
-// and we'll also be adding 3 method aliases for function, class, and null
-declare interface assert extends Clone<typeof $assert> {
-  /**
-   * Asserts that a condition is true.
-   * @param condition the condition to assert
-   * @param description the message to throw if the condition is false
-   * @category Types
-   * @example ```ts
-   * import { is} from "https://deno.land/x/dis@0.0.1/mod.ts";
-   *
-   * is.assert(true, "true is true");
-   * ```
-   */
-  (condition: boolean, description?: string): asserts condition;
-  /**
-   * Asserts that a condition is true.
-   * @param condition the condition to assert
-   * @param description the message to throw if the condition is false
-   * @category Types
-   * @example ```ts
-   * import { is} from "https://deno.land/x/dis@0.0.1/mod.ts";
-   *
-   * is.assert(true, "true is true");
-   * ```
-   */
-  (
-    condition: boolean,
-    description: string,
-    value: unknown,
-    options?: AssertOptions,
-  ): asserts condition;
-  class: (value: unknown) => asserts value is Class<unknown, any[]>;
-  function: (value: unknown) => asserts value is Function;
-  null: (value: unknown) => asserts value is null;
+import type { DecodedEntityTag, DecodedStatTag } from "./mod.d.ts";
+
+export function decodeStatTag(value: string): Partial<DecodedStatTag> {
+  const [size, time] = value.split("-").map((n: string | number) => (
+    n = parseInt(n as string, ETAG_RADIX) as number, is.nan(n) ? 0 : n
+  ));
+  const mtime = new Date(time);
+  return { size, mtime, hash: undefined };
 }
 
-// similar to above, defining the new shape of the "is" object (which includes
-// an assert property that contains the whole assert object from above)
-// we'll also be adding the same 3 method aliases for func/class/null
-declare interface is extends Noop<typeof $is> {
-  assert: assert;
-  function: (value: unknown) => value is Function;
-  class: (value: unknown) => value is Class<unknown, any[]>;
-  null: (value: unknown) => value is null;
-  request: (value: unknown) => value is Request;
-  response: (value: unknown) => value is Response;
+export function decodeEntityTag(value: string): Partial<DecodedEntityTag> {
+  const [length, hash] = value.split("-");
+  let size = parseInt(length, ETAG_RADIX);
+  if (is.nan(size)) size = 0;
+  return { size, hash, mtime: undefined };
 }
-
-// the callable assert function, exposed as is.assert() / assert()
-function assertFn(
-  condition: boolean,
-  description?: string,
-  value?: unknown,
-  options: { multipleValues?: boolean } = {},
-): asserts condition {
-  if (!condition) {
-    const { multipleValues = false } = options;
-    if (description == null) {
-      throw new AssertionError(
-        "Assertion of type failed! Received an unexpected value.",
-      );
-    }
-    const valuesMessage = (multipleValues && is.array(value))
-      ? `received values of types ${
-        [
-          ...new Set(
-            (value as any[]).map((singleValue) => `\`${is(singleValue)}\``),
-          ),
-        ].join(", ")
-      }`
-      : `received value of type \`${is(value)}\``;
-
-    throw new TypeError(
-      `Expected value which is \`${description}\`, ${valuesMessage}.`,
-    );
-  }
-}
-
-// assign all the properties of the original assert object to the new assertFn
-// and add the three aliases while we're at it too
-export const assert: assert = Object.assign(assertFn, $assert, {
-  function: $assert.function_,
-  class: $assert.class_,
-  null: $assert.null_,
-});
-
-// assign the new properties to the original $is function / object
-// and export it as the desired final name: 'is'
-export const is: is = Object.assign($is as is, {
-  request: (value: unknown): value is Request => (value instanceof Request),
-  response: (value: unknown): value is Response => (value instanceof Response),
-});
-
-export { type Assert };
-
-type Algo = DigestAlgorithm;
 
 /** @internal */
-export function digestSync(entity: Entity, algo: Algo = "SHA-1"): string {
+export function digestSync(
+  entity: Entity,
+  DigestAlgorithm: DigestAlgorithm = "SHA-1",
+): string {
   /** @internal */
   function validateEntity(entity: Entity): Uint8Array {
     if (is.uint8Array(entity) || is.arrayBuffer(entity)) {
@@ -139,20 +54,29 @@ export function digestSync(entity: Entity, algo: Algo = "SHA-1"): string {
       // encode as (utf-8) Uint8Array
       return new TextEncoder().encode(entity);
     }
-    is.assert(entity != null, "Entity cannot be null or undefined!");
+
+    assert(
+      !is.nullOrUndefined(entity),
+      "Entity cannot be null or undefined!",
+      entity,
+    );
+
     return entity as any;
   }
 
   const toHex = (v: number) => v.toString(16).padStart(2, "0");
 
   const buffer = validateEntity(entity);
-  const hash = crypto.subtle.digestSync(algo, buffer);
+  const hash = crypto.subtle.digestSync(DigestAlgorithm, buffer);
 
   return base64.encode(Array.from(new Uint8Array(hash), toHex).join(""));
 }
 
-export function digest(entity: Entity, algo: Algo = "SHA-1"): Promise<string> {
-  return Promise.resolve(digestSync(entity, algo));
+export function digest(
+  entity: Entity,
+  DigestAlgorithm: DigestAlgorithm = "SHA-1",
+): Promise<string> {
+  return Promise.resolve(digestSync(entity, DigestAlgorithm));
 }
 
 /** @internal */
@@ -164,12 +88,12 @@ export function isFileInfo(obj: any): obj is FileInfo | Stats {
   // deno-fmt-ignore
   return obj
     && is.nonEmptyObject(obj)
-    // && "ctime" in obj
-    && "mtime" in obj
     && "ino" in obj
     && "size" in obj
-    && (is.any([is.date, is.string, is.number], obj.mtime))
+    && "mtime" in obj
+    && "ctime" in obj
     && is.any(is.number, obj.ino, obj.size)
+    && is.any([is.date, is.string, is.number], obj.mtime)
     && is.number((obj as any).size);
 }
 
@@ -177,13 +101,15 @@ export function isFileInfo(obj: any): obj is FileInfo | Stats {
 export function fstatSync<
   T extends VirtualFsFile["rid"] | VirtualFsFile,
 >(file: T): VirtualFileInfo;
-/** @internal */
-export function fstatSync(file: Deno.FsFile): Deno.FileInfo;
-/** @internal */
-export function fstatSync(file: string | URL | Stats): FileInfo;
 
 /** @internal */
-export function fstatSync(file: any): any {
+export function fstatSync(file: Deno.FsFile): FileInfo;
+
+/** @internal */
+export function fstatSync(file: string | URL | Stats): Partial<FileInfo>;
+
+/** @internal */
+export function fstatSync(file: any): any | undefined {
   // scaffolding for virtual fs support in the future
   if ("body" in file && "meta" in file) {
     return file.meta;
@@ -207,20 +133,15 @@ export function fstatSync(file: any): any {
 }
 
 /** @internal */
-export function fstat<
-  T extends VirtualFsFile["rid"] | VirtualFsFile,
->(file: T): Promise<VirtualFileInfo>;
-
-/** @internal */
-export function fstat(file: Deno.FsFile): Promise<FileInfo>;
-
-/** @internal */
-export function fstat(file: string | URL | Stats): Promise<FileInfo>;
-
-/** @internal */
-export function fstat(file: any): Promise<any> {
+export function fstat(
+  file:
+    | (string | URL | Stats)
+    | Deno.FsFile
+    | (number | VirtualFsFile)
+    | FileInfo,
+): Promise<ReturnType<typeof fstatSync>> {
   try {
-    return Promise.resolve(fstatSync(file));
+    return Promise.resolve(fstatSync(file as any));
   } catch (err) {
     return Promise.reject(err);
   }
@@ -236,10 +157,9 @@ export async function getEntity<C extends Context<S>, S = State>(
 ): Promise<Entity>;
 
 /** @internal */
-export async function getEntity<T extends Response>(res: T): Promise<Entity>;
-
-/** @internal */
-export async function getEntity<T extends BodyInit>(body: T): Promise<Entity>;
+export async function getEntity<T extends Response | BodyInit>(
+  res: T,
+): Promise<Entity>;
 
 /** @internal */
 export async function getEntity(ctx: any): Promise<any> {
@@ -254,7 +174,7 @@ export async function getEntity(ctx: any): Promise<any> {
         return (ctx as Context).response.arrayBuffer()!;
       } else if (is.response(ctx)) {
         if ((ctx as Response).bodyUsed) {
-          throw new ERR_STREAM_ALREADY_FINISHED("getEntity(ctx.arrayBuffer");
+          throw new ERR_STREAM_ALREADY_FINISHED("getEntity(ctx.arrayBuffer)");
         }
         return (ctx as Response).arrayBuffer()!;
       } else if (is.directInstanceOf(ctx, Deno.FsFile) || isFileInfo(ctx)) {
@@ -277,9 +197,10 @@ export async function getEntity(ctx: any): Promise<any> {
 
     return undefined;
   } catch (cause) {
-    throw new Error(`Unable to determine entity body from given context!`, {
-      cause,
-    });
+    throw new TypeError(
+      `Unable to determine entity body from given context!`,
+      { cause },
+    );
   }
 }
 
