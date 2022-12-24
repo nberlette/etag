93f1206e6c0916a0044c069d5e9d68926a0d56ee
Nicholas Berlette <nick@berlette.com>
2022-12-24 09:36:51 -0800

    chore(deps): update dependencies

--- deps.ts
+++ deps.ts
@@ -2,21 +2,19 @@ export {
   fstatSync,
   Stats,
   statSync,
-} from "https://deno.land/std@0.158.0/node/fs.ts";
+} from "https://deno.land/std@0.170.0/node/fs.ts";
 
-export * from "https://deno.land/std@0.158.0/node/buffer.ts";
+export * from "https://deno.land/std@0.170.0/node/buffer.ts";
 
-export * from "https://deno.land/x/dis@0.0.1/mod.ts";
+export * from "https://deno.land/x/dis@0.2.0/mod.ts";
 
-export * as base64 from "https://deno.land/std@0.158.0/encoding/base64.ts";
+export * as base64 from "https://deno.land/std@0.170.0/encoding/base64.ts";
 
-export { crypto } from "https://deno.land/std@0.158.0/crypto/mod.ts";
+export { crypto, type DigestAlgorithm } from "https://deno.land/std@0.170.0/crypto/mod.ts";
 
-export { type DigestAlgorithm } from "https://deno.land/std@0.158.0/crypto/_wasm_crypto/mod.ts";
-
-export { AssertionError } from "https://deno.land/std@0.158.0/testing/asserts.ts";
+export { AssertionError } from "https://deno.land/std@0.170.0/testing/asserts.ts";
 
 export {
   ERR_STREAM_ALREADY_FINISHED,
   ERR_STREAM_DESTROYED,
-} from "https://deno.land/std@0.158.0/node/internal/errors.ts";
+} from "https://deno.land/std@0.170.0/node/internal/errors.ts";
