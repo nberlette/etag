d6249c5bf2264f0e5ea8edef27bcb9a014b2d99c
Nicholas Berlette <nick@berlette.com>
2022-12-24 13:23:36 -0800

    deps: add dax + cliffy ansi for publish task

--- deps.ts
+++ deps.ts
@@ -6,13 +6,29 @@ export {
 
 export * from "https://deno.land/std@0.170.0/node/buffer.ts";
 
-export * from "https://deno.land/x/dis@0.2.0/mod.ts";
+export {
+  type Assert,
+  assert,
+  is,
+  type TypeName,
+} from "https://x.nest.land/dis@0.3.0-rc.3/mod.ts";
+
+export { type GetTypeName } from "https://x.nest.land/dis@0.3.0-rc.3/types.ts";
+
+export { $ } from "https://deno.land/x/dax@0.17.0/mod.ts";
+
+export { colors } from "https://deno.land/x/cliffy@v0.25.6/ansi/mod.ts";
 
 export * as base64 from "https://deno.land/std@0.170.0/encoding/base64.ts";
 
-export { crypto, type DigestAlgorithm } from "https://deno.land/std@0.170.0/crypto/mod.ts";
+export {
+  crypto,
+  type DigestAlgorithm,
+} from "https://deno.land/std@0.170.0/crypto/mod.ts";
 
-export { AssertionError } from "https://deno.land/std@0.170.0/testing/asserts.ts";
+export {
+  AssertionError,
+} from "https://deno.land/std@0.170.0/testing/asserts.ts";
 
 export {
   ERR_STREAM_ALREADY_FINISHED,
